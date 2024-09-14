5b18a1fd66a4f4e1a7174de18660296f3a650472
rebeccaz3 <rebecca.zhang.1@stonybrook.edu>
2024-09-13 22:11:52 -0400

    Update Project1_Part2.m

--- Project1_Part2.m
+++ Project1_Part2.m
@@ -3,7 +3,7 @@ precision_level = input('Enter the number of significant figures for level of pr
 
 % Initialize variables  
 points_in_circle = 0;
-total_points = 0; % Keeps count of  number of iterations required to achieve each level of precision
+total_points = 0; % Keeps count of  number of iterations
 deviation = inf; % Start with a large deviation to enter the loop
 
 % While loop to estimate pi until the desired precision is achieved
@@ -11,7 +11,7 @@ while deviation > 0.1^precision_level
     % Generate random points x and y in the range [-1, 1] 
     x = 2 * rand() - 1;
     y = 2 * rand() - 1;
-    total_points = total_points + 1; % Increment the total points counter
+    total_points = total_points + 1; 
 
     % Check if the point is inside/outside the circle
     if x^2 + y^2 <= 1
